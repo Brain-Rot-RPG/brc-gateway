3913bc5a9ac9cf71ab76233ef4dbf0014c04baa1
Ulysse HOLZINGER <ulysseholzinger@gmail.com>
2026-02-12 20:36:48 +0100

    Fixes error

--- db/brc-battle/init.sql
+++ db/brc-battle/init.sql
@@ -1,6 +1,25 @@
-CREATE TABLE Battle (
+CREATE TABLE battles (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     choice TEXT[],
     checkFunction TEXT
+);
+
+-- insert a rock paper scissor
+INSERT INTO battles (name, choice, checkFunction) VALUES (
+    'Rock Paper Scissors',
+    ARRAY['rock', 'paper', 'scissors'],
+    'function checkRPS(playerChoice, computerChoice) {
+        if (playerChoice === computerChoice) {
+            return "It's a tie!";
+        } else if (
+            (playerChoice === "rock" && computerChoice === "scissors") ||
+            (playerChoice === "paper" && computerChoice === "rock") ||
+            (playerChoice === "scissors" && computerChoice === "paper")
+        ) {
+            return "You win!";
+        } else {
+            return "Computer wins!";
+        }
+    }'
 );
